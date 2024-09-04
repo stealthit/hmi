f247846e206739435e08d3a8914bfe69ce251c4d
ganachecell <bluegag@gmail.com>
2024-09-04 21:31:48 +0900

    popup 완료

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
-function closePopup(event) {
-  const bg = document.getElementById("bg-popup");
+function closePopup1(event) {
+  const bg = document.getElementById("bg-popup1");
   // const name = document.querySelector(".popup-wrap");
   var parent = event.target.parentElement.parentElement;
 
@@ -10,11 +10,33 @@ function closePopup(event) {
   parent.classList.add('off');
 }
 
-function openPopup(str)  {
-  const bg = document.getElementById("bg-popup");
+function openPopup1(str)  {
+  const bg = document.getElementById("bg-popup1");
   const name = document.querySelector("." + str);
 
   if (name.classList.contains('popup-wrap')){
+    bg.classList.add('on');
+    name.classList.add('on');
+  }
+}  
+
+function closePopup2(event) {
+  const bg = document.getElementById("bg-popup2");
+  // const name = document.querySelector(".popup-wrap");
+  var parent = event.target.parentElement.parentElement;
+
+  bg.classList.remove('on');
+  parent.classList.remove('on');
+
+  bg.classList.add('off');
+  parent.classList.add('off');
+}
+
+function openPopup2(str)  {
+  const bg = document.getElementById("bg-popup2");
+  const name = document.querySelector("." + str);
+
+  if (name.classList.contains('popup-wrap2')){
     bg.classList.add('on');
     name.classList.add('on');
   }
